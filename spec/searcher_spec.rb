ec7aaa20a6055028638facadb635b9990084a29f
Tom van Leeuwen <tom.van.leeuwen@saasplaza.com>
2014-01-11 09:36:14 +0100

    using pathname now so should work on windows

--- spec/searcher_spec.rb
+++ spec/searcher_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 require 'digest/md5'
+require 'tmpdir'
+require 'fileutils'
+require 'pathname'
 
 describe Searcher do
-  before :all do
+  before do
     @episode = Episode.new 'Sons.of.Anarchy.S06E13.720p.HDTV.x264-2HD.mkv'
     @searcher = Searcher.new(@episode)
+
+    @tmpdir = Pathname.new Dir::mktmpdir
   end
+
+  after { FileUtils.remove_dir(@tmpdir) }
 
   it 'should be able to find links' do
     @searcher.links.should_not be_empty
@@ -15,15 +22,15 @@ describe Searcher do
   end
 
   it 'should be able to download links' do
-    @searcher.download_links('/tmp')
+    @searcher.download_links(@tmpdir)
 
     files = [
       {
-          :path => "/tmp/Sons of Anarchy - 06x13 - A Mother's Work.WEB-DL.English.orig.srt",
+          :path => @tmpdir.join("Sons of Anarchy - 06x13 - A Mother's Work.WEB-DL.English.orig.srt"),
           :md5  => 'c9c11b17ade210292e635e7389e6fee2'
       },
       {
-          :path => "/tmp/Sons of Anarchy.S06E13.HDTV.x264-2HD.srt",
+          :path => @tmpdir.join("Sons of Anarchy.S06E13.HDTV.x264-2HD.srt"),
           :md5  => 'aa42816715c781d41606a167d1677b8f'
       }
     ]
